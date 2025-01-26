09ad9e0598ce2cdd10f5378ad772e3202b9b5ec9
Rafael Grisotto <rafael.grisotto@unisoma.com>
2025-01-26 16:39:53 -0300

    added vim-jack-in

---
+++ .config/nvim/fnl/plugins/vim-jack-in.fnl
@@ -0,0 +1,4 @@
+[{1 :clojure-vim/vim-jack-in
+  :dependencies [:tpope/vim-dispatch
+                 :radenling/vim-dispatch-neovim]
+  }]
---
+++ .config/nvim/lua/plugins/vim-jack-in.lua
@@ -0,0 +1,2 @@
+-- [nfnl] Compiled from fnl/plugins/vim-jack-in.fnl by https://github.com/Olical/nfnl, do not edit.
+return {{"clojure-vim/vim-jack-in", dependencies = {"tpope/vim-dispatch", "radenling/vim-dispatch-neovim"}}}
