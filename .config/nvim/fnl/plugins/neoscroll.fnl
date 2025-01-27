7c398ee811d12d7c33072c3468692d259590e2e8
Rafael Grisotto <rafael.grisotto@unisoma.com>
2025-01-27 09:00:44 -0300

    added neoscroll

---
+++ .config/nvim/fnl/plugins/neoscroll.fnl
@@ -0,0 +1,3 @@
+[{1 :karb94/neoscroll.nvim
+  :opts {:easing_function :quadratic
+         :mappings ["<C-u>" "<C-d>" "<C-b>" "<C-f>"]}}]
---
+++ .config/nvim/lua/plugins/neoscroll.lua
@@ -0,0 +1,2 @@
+-- [nfnl] Compiled from fnl/plugins/neoscroll.fnl by https://github.com/Olical/nfnl, do not edit.
+return {{"karb94/neoscroll.nvim", opts = {easing_function = "quadratic", mappings = {"<C-u>", "<C-d>", "<C-b>", "<C-f>"}}}}
