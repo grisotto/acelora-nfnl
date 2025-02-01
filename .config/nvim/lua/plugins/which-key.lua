8a916bf71cc2deeec247bedf5965fddf13546a6d
Rafael Grisotto <rafael.grisotto@unisoma.com>
2025-02-01 17:27:47 -0300

    updated which-key

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,10 +1,6 @@
 -- [nfnl] Compiled from fnl/plugins/which-key.fnl by https://github.com/Olical/nfnl, do not edit.
 local function _1_()
   local which_key = require("which-key")
-  return which_key.setup({keys = {"<leader>", "<c-w>", "\"", "'", "`", "c", "v", "g"}, cmd = "WhichKey"})
+  return which_key.setup({})
 end
-local function _2_()
-  vim.api.nvim_set_keymap("n", "<leader>wK", "<cmd>WhichKey <CR>", {desc = "whichkey all keymaps"})
-  return vim.api.nvim_set_keymap("n", "<leader>wk", "(fn [] (vim.cmd (.. \"WhichKey \" (vim.fn.input \"WhichKey: \"))))", {desc = "whichkey query lookup"})
-end
-return {{"folke/which-key.nvim", config = _1_, init = _2_}}
+return {{"folke/which-key.nvim", event = "VeryLazy", config = _1_}}
