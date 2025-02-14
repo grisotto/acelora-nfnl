0bfe3e6f7682749339f5ab20a60cc19e44e39686
Rafael Grisotto <rafael.grisotto@unisoma.com>
2025-02-14 10:08:25 -0300

    moved copilotcat to copilotchat lol

--- .config/nvim/fnl/plugins/copilotcat.fnl
+++
@@ -1,8 +0,0 @@
-[{1 :CopilotC-Nvim/CopilotChat.nvim
-  :lazy false
-  :build "make tiktoken"
-  :dependencies [:github/copilot.vim
-                 :nvim-lua/plenary.nvim]
-  :config (fn []
-            (let [CopilotChat (require :CopilotChat)]
-              (CopilotChat.setup ))) }]
--- .config/nvim/lua/plugins/copilotcat.lua
+++
@@ -1,6 +0,0 @@
--- [nfnl] Compiled from fnl/plugins/copilotcat.fnl by https://github.com/Olical/nfnl, do not edit.
-local function _1_()
-  local CopilotChat = require("CopilotChat")
-  return CopilotChat.setup()
-end
-return {{"CopilotC-Nvim/CopilotChat.nvim", build = "make tiktoken", dependencies = {"github/copilot.vim", "nvim-lua/plenary.nvim"}, config = _1_, lazy = false}}
